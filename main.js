832a7efaeeb826729fca55e1f04bf6a931d65750
Kavyansh312009 <94540332+Kavyansh312009@users.noreply.github.com>
2022-10-07 21:31:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+status1 = "";
+video = "";
+
+function preload(){
+    video= createVideo('video.mp4');
+}
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+    video.hide();
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detcting Objects";
+}
+function modelLoaded(){
+    console.log("hehe BOI!");
+    status1 = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+function draw(){
+    image(video,0,0,480,380)
+}
