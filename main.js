a9011bb1f70b155f3e890df864a4fe5d8001ff7b
Kavyansh312009 <94540332+Kavyansh312009@users.noreply.github.com>
2022-10-11 19:36:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status1 = "";
 video = "";
+objects= [];
 
 function preload(){
     video= createVideo('video.mp4');
@@ -20,6 +21,26 @@ function modelLoaded(){
     video.speed(1);
     video.volume(0);
 }
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }console.log(results);
+    objects = results;
+}
 function draw(){
-    image(video,0,0,480,380)
+    image(video,0,0,480,380);
+    if(status1 == true){
+        objectDetector.detect(video,gotResults);
+        for(K = 0; K < objects.length; K++){
+            document.getElementById("status").innerHTML = "Status: Objects detected";
+            document.getElementById("no._of_obj").innerHTML = "No. of Objects detected are: "+objects.length;
+
+            fill("deepskyblue");
+            percent = floor(objects[K].confidence * 100);
+            text(objects[K].label + "" + percent + "%", objects[K].x + 15, objects[K].y + 15);
+            noFill("");
+            stroke("gold");
+            rect(objects[K].x, objects[K].y, objects[K].width, objects[K].height);
+        }
+}
 }
